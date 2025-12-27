359ccab4cc4baf20b99a4d2ca19807474cc229b3
Kath13 <veg19xxr@list.ru>
2025-12-27 13:29:51 +0200

    Simplify class names

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,15 +3,15 @@ import './Card.css'
 export const Card = ({title, price, address, date, imageURL}) => {
     return (
         <>
-            <a href="/product.html" className="content-main__list-item">
-                <div className="content-main__list-item--img">
+            <a href="/product.html" className="card">
+                <div className="card--img">
                     <img src={`/image/${imageURL}`} alt={`Изображение товара ${title}`}/>
                 </div>
-                <h5 className="content-main__list-item--title">{title}</h5>
-                <strong className="content-main__list-item--price">{price} ₽</strong>
-                <div className="content-main__list-item--desc-box">
-                    <span className="content-main__list-item--desc">{address}</span>
-                    <span className="content-main__list-item--desc">{date}</span>
+                <h5 className="card--title">{title}</h5>
+                <strong className="card--price">{price} ₽</strong>
+                <div className="card--desc-box">
+                    <span className="card--desc">{address}</span>
+                    <span className="card--desc">{date}</span>
                 </div>
             </a>
         </>
