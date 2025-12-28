0f0cb3fba84c0f9cbbddf3856ddcdc73561eb01e
Kath13 <veg19xxr@list.ru>
2025-12-28 16:00:19 +0200

    Add Product page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Home} from './pages/Home';
+import {createBrowserRouter} from 'react-router';
+import {RouterProvider} from 'react-router/dom';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/Product.jsx
@@ -0,0 +1,60 @@
+import {Header} from '../components/Header/Header';
+import {Services} from '../components/Services/Services';
+import {Footer} from '../components/Footer/Footer';
+
+export const Product = () => {
+    return (
+        <>
+            <Header/>
+            <main>
+                <section className="search">
+                    <div className="container">
+                        <div className="search-box">
+                            <input type="text" placeholder="Поиск по объявлениям"/>
+                            <button className="btn btn-primary search-btn">
+                                <img src="public/image/search.svg" alt="Search" className="search-btn__icon"/>
+                                <span className="search-btn__text">Найти</span>
+                            </button>
+                        </div>
+                    </div>
+                </section>
+                <section className="content">
+                    <div className="container">
+                        <div className="content-box">
+                            <div className="content-product">
+                                <div className="content-product__info-box">
+                                    <h3 className="content-product__title">
+                                        Электросамокат kugoo Gx
+                                    </h3>
+                                    <img className="content-product__img" src="public/image/product.png"
+                                         alt="Product image"/>
+                                    <div className="content-product__desc">
+                                        <p>Продаю не спеша самокат в хорошем состоянии.</p>
+                                        <p>Торг возможен.</p>
+                                        <p>За период эксплуатации не выявлено ни одной проблемы.</p>
+                                        <p>Из минусов — нужно прокачать задний тормоз.</p>
+                                        <p>Установлен отсекатель сзади.</p>
+                                        <p>Покрышки CST внедорожные.</p>
+                                        <p>Все на подшипниках, болты протянуты.</p>
+                                        <p>Пробег 881км , это немного для такого зверя.</p>
+                                    </div>
+                                </div>
+                                <div className="content-product__summary-box">
+                                    <span className="content-product__price">75 000 ₽</span>
+                                    <button className="content-product__show-phone-btn btn btn-primary">Показать телефон
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="content-side">
+                                <div className="content-side__box">
+                                    <Services />
+                                    <Footer />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
